1ff0788e4c3f9f516c1f30531f0e44463408bc11
isaac3597 <isaacchebii3597@gmail.com>
2023-06-22 01:43:33 -0700

    Drawing of a new triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - will be able to prints triangle, then followed by a new line
+ * @size: size of the triangle to be drawn
+ */
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
