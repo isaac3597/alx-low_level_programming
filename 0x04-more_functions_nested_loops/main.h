522d0837b21b3e1509a25125fb02c8aee1557d55
isaac3597 <isaacchebii3597@gmail.com>
2023-06-22 00:47:39 -0700

    Deletion

--- 0x04-more_functions_nested_loops/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
-
-int _isdigit(int c);
-int _isupper(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-#endif
--- README.md
+++
@@ -1,1 +0,0 @@
-README.md file for the descriptions of the C program files 
