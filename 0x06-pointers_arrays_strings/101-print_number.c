d6174d40c0de9df3108d1947d621c9d193868cc8
isaac3597 <isaacchebii3597@gmail.com>
2023-06-29 02:51:08 -0700

    New changes

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,27 +1,25 @@
 #include "main.h"
 
 /**
- * print_number - For printing of the  numbers in chars
- * @n: integer params.
- * Return: 0
+ * print_number - will be able to print an integer;
+ * @n: integer to be printed;
  */
-
 void print_number(int n)
 {
 	unsigned int n1;
 
-	n1 = n;
-
 	if (n < 0)
 	{
-		_putchar('-');
 		n1 = -n;
+		_putchar('-');
+	}
+	else
+	{
+		n1 = n;
 	}
 
-	if (n1 / 10 != 0)
-	{
+	if (n1 / 10)
 		print_number(n1 / 10);
-	}
+
 	_putchar((n1 % 10) + '0');
 }
-
